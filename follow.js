1a0626694664d654ca6cb564215018044db78d82
mattierocks <mattierocks@gmail.com>
2017-11-03 13:37:05 -0400

    add follow function

---
+++ follow.js
@@ -0,0 +1,28 @@
+var Twitter = require('twitter');
+var config = require('./config.js');
+var T = new Twitter(config);
+
+// Set up search parameters
+var params = {
+    q: '@mattierocks',
+    count: 10,
+    result_type: 'recent',
+    lang: 'en'
+}
+
+T.get('search/tweets', params, function(err, data, response) {
+    if (!err) {
+        for (let i = 0; i < data.statuses.length; i++) {
+            let screen_name = data.statuses[i].user.screen_name;
+            T.post('friendships/create', { screen_name }, function(err, res) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    console.log(screen_name, ': **FOLLOWED**');
+                }
+            });
+        }
+    } else {
+        console.log(err);
+    }
+})
